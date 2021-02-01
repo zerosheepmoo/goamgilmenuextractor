3c86dbd1f113398562dbc11852f84a2007d03354
zerosheepmoo <zerosheepmoo@gmail.com>
2021-02-02 01:53:45 +0900

    치명적 논리오류 수정

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let textMemo = new TextMemo(document.getElementById('text-memo'));
     textMemo.innerHTML = '<h2>도움말(스크롤하세요...)</h2>\
     <p>메뉴의 배경색에 따라 변경된 날짜를 알 수 있습니다!</p>\
-    <p><span style="color:rgb(125, 132, 255);">보라색</span>: 7일 이내</p>\
-    <p><span style="color:rgb(250, 141, 250);">분홍색</span>: 3일 이내</p>\
-    <p><span style="color:rgb(255, 112, 112);">빨간색</span>: 오늘</p>\
+    <p><span style="color:rgb(125, 132, 255);">보라색</span>: 7일 이하</p>\
+    <p><span style="color:rgb(250, 141, 250);">분홍색</span>: 3일 이하</p>\
+    <p><span style="color:rgb(255, 112, 112);">빨간색</span>: 어제~오늘</p>\
     <p>수정 내역 보기에서 무엇을 수정했는지 알아보세요!</p>\
     <p><span style="color:yellow;">노란색</span>은 쿠팡이츠 어플에 잘못 등록되어 있는 경우!</p>\
     <p>클릭하면 닫힙니다.</p>\
